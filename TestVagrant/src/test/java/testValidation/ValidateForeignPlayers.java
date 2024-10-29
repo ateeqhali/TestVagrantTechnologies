defad307067af3f020272754f6cfce5374ad2537
ateeqhali <103210649+ateeqhali@users.noreply.github.com>
2024-10-29 10:17:37 +0530

    Update ValidateForeignPlayers.java

--- TestVagrant/src/test/java/testValidation/ValidateForeignPlayers.java
+++ TestVagrant/src/test/java/testValidation/ValidateForeignPlayers.java
@@ -32,7 +32,7 @@ public class ValidateForeignPlayers {
 		}
 		// Validating the 4 foreign player
 		Assert.assertEquals(4, countForiegnPlayer);
-
+//comment from remote
 		if (countForiegnPlayer == 4) {
 			System.out.println("Royal Challengers Bangalore has 4 foriegn player");
 		} else {
